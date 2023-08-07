074215551e7f894fd422d8e200ad140806324f01
Humberto Pereira <humberto_pereira@live.com>
2023-08-07 13:06:29 +0000

    commit

--- map.js
+++ map.js
@@ -5,35 +5,51 @@
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5]
+let results = [];
+for (let num of nums){
+    results.push(num * 2);
+};
+console.log(results)
 
+// --------------------------------Using map()
 
-// Using map()
+const multByTwo = function (num) {
+    return num * 2;
+}
 
+const mapResults = nums.map(multByTwo);
+console.log(mapResults)
 
-// Simplified w/ map()
-
+// --------------------------------Simplified w/ map()
+const simplified = nums.map(function (num) {return num * 2});
+console.log(simplified)
 
 // Simplfied w/ map() + arrow function
-
+const simplifiedArrow = nums.map(num => num * 2);
+console.log(simplifiedArrow)
 
 // With objects:
 const students = [
-  {
-    id: 1,
-    name: 'Mark',
-    profession: 'Developer',
-    skill: 'JavaScript'
-  },
-  {
-    id: 2,
-    name: 'Ariel',
-    profession: 'Developer',
-    skill: 'HTML'
-  },
-  {
-    id: 3,
-    name: 'Jason',
-    profession: 'Designer',
-    skill: 'CSS'
-  },
+    {
+        id: 1,
+        name: 'Mark',
+        profession: 'Developer',
+        skill: 'JavaScript'
+    },
+    {
+        id: 2,
+        name: 'Ariel',
+        profession: 'Developer',
+        skill: 'HTML'
+    },
+    {
+        id: 3,
+        name: 'Jason',
+        profession: 'Designer',
+        skill: 'CSS'
+    },
 ];
+
+const studentsWithId = students.map(student => [student.name, student.id])
+console.log(studentsWithId)
